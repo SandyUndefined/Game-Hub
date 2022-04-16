c45457d26126300aad18fefa808a031df1b461c3
Sandy_undefined <08sandysk@gmail.com>
2022-04-16 23:58:08 +0530

    added files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  interstitial_ad_id : "ca-app-pub-4279390018890538/5604202355",
+  testing_ad: false,
+  support_email: "sandy.undefined@gmail.com",
+  support_phone: "+917318724249",
+  support_url: "https://github.com/SandyUndefined/Hyper-Games",
+  about_app: "Hyper Games is a new all in one app where you can play all online mini-games and html games. Usually game apps are comes with only one game. But, this game app contains with lots of games which is really powerful and smooth gaming experience. This application have some special features, It’s stores all game data on device with very less. the total app size is less than 7.5 Mb for android. Currently it contains more than 50 Games.",
+  app_version:"v1.0.0"
+};
